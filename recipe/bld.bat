fea95071e1f6ba75965ca581e17ac5da6c871b82
Michael Sarahan <msarahan@gmail.com>
2016-06-02 19:24:03 -0500

    update to 8.6.5; add tix

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,15 +4,19 @@ if "%ARCH%"=="32" (
   set MACHINE="AMD64"
 )
 
+set "TIX_VERSION=8.4.3"
+
 :: Get the major minor version info (e.g. `8_5`) so it can be used in the URL.
 python -c "import os; print('_'.join(os.environ['PKG_VERSION'].split('.')[:2]))" > temp.txt
 set /p MAJ_MIN_VER=<temp.txt
 
 curl -L -o tcl%PKG_VERSION%.tar.gz "ftp://ftp.tcl.tk/pub/tcl/tcl%MAJ_MIN_VER%/tcl%PKG_VERSION%-src.tar.gz"
 curl -L -o tk%PKG_VERSION%.tar.gz "ftp://ftp.tcl.tk/pub/tcl/tcl%MAJ_MIN_VER%/tk%PKG_VERSION%-src.tar.gz"
+curl -L -o tix%TIX_VERSION%.tar.gz "https://sourceforge.net/projects/tix/files/tix/%TIX_VERSION%/Tix%TIX_VERSION%-src.tar.gz/download"
 
 7za x -so tcl%PKG_VERSION%.tar.gz | 7za x -si -aoa -ttar
 7za x -so tk%PKG_VERSION%.tar.gz | 7za x -si -aoa -ttar
+7za x -so tix%TIX_VERSION%.tar.gz | 7za x -si -aoa -ttar
 
 cd tcl%PKG_VERSION%\win
 nmake -f makefile.vc all install INSTALLDIR=%LIBRARY_PREFIX% MACHINE=%MACHINE%
@@ -23,6 +27,7 @@ REM If you don't have this path, you are missing the Windows 7 SDK.  Please inst
 REM   NOTE: Later SDKs remove tmschema.h.  It really is necessary to use the Win 7 SDK.
 set INCLUDE=%INCLUDE%;c:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Include
 
+:: Tk build
 
 cd ..\..\tk%PKG_VERSION%\win
 nmake -f makefile.vc all install INSTALLDIR=%LIBRARY_PREFIX% MACHINE=%MACHINE% TCLDIR=..\..\tcl%PKG_VERSION%
@@ -33,4 +38,13 @@ python -c "import os; print(''.join(os.environ['PKG_VERSION'].split('.')[:2]))" 
 set /p MAJ_MIN_VER=<temp.txt
 
 :: Make sure that `wish` can be called without the version info.
-copy %LIBRARY_PREFIX%\bin\wish%MAJ_MIN_VER%.exe %LIBRARY_PREFIX%\bin\wish.exe
+copy %LIBRARY_PREFIX%\bin\wish%MAJ_MIN_VER%t.exe %LIBRARY_PREFIX%\bin\wish.exe
+
+:: Tix build
+
+cd ..\..\tix-%TIX_VERSION%\
+bash -c "./configure --prefix=%LIBRARY_PREFIX:\=/% --enable-shared --with-tcl=%LIBRARY_LIB:\=/%"
+bash -c "make && make install"
+
+:: overwrite an incorrect wish version here
+copy %LIBRARY_PREFIX%\bin\wish%MAJ_MIN_VER%t.exe %LIBRARY_PREFIX%\bin\wish%MAJ_MIN_VER%.exe
